46d721ffb86997713db3df150c942a5444d49ad4
Harsh <replytobambam@gmail.com>
2024-11-06 22:42:26 +0530

    some small changes in the code and rechecking

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,2 +1,126 @@
 import sys
-from data
+from dataclasses import dataclass
+
+import pandas as pd
+import numpy as np 
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder , StandardScaler
+import os 
+
+from src.utils import save_object
+
+
+
+@dataclass 
+class Datatransformationconfig:
+    preprocessor_obj_file = os.path.join('artifacts' ,'preprocessing.pkl')
+
+
+class Datatransformation :
+    def __init__(self):
+        self.data_transformation_config = Datatransformationconfig()
+
+    def get_data_transformer_object(self):
+        '''
+        This function si responsible for data trnasformation
+        
+        '''
+       
+        numerical_columns = ["writing_score", "reading_score"]
+        categorical_columns = [
+                "gender",
+                "race_ethnicity",
+                "parental_level_of_education",
+                "lunch",
+                "test_preparation_course",
+            ]
+
+        num_pipeline= Pipeline(
+                steps=[
+                ("imputer",SimpleImputer(strategy="median")),
+                ("scaler",StandardScaler())
+
+                ]
+            )
+
+        cat_pipeline=Pipeline(
+
+                steps=[
+                ("imputer",SimpleImputer(strategy="most_frequent")),
+                ("one_hot_encoder",OneHotEncoder()),
+                ("scaler",StandardScaler(with_mean=False))
+                ]
+
+            )
+
+           
+
+        preprocessor=ColumnTransformer(
+                [
+                ("num_pipeline",num_pipeline,numerical_columns),
+                ("cat_pipelines",cat_pipeline,categorical_columns)
+
+                ]
+
+
+            )
+
+        return preprocessor
+
+
+
+
+def initaie_data_transformation(self , train_path , test_path ):
+        
+        train_df = pd.read_csv(train_path)
+        test_df = pd.read_csv(test_path) 
+        # loaded the the Traning and testing Data into the Function 
+
+
+
+        prerpocessing_obj = self.get_data_transformer_obj() # called the Object that will do the preprocessing on the data 
+
+        target_column_name = "math_score" # created the target Column that is supoosed to pe pridicted 
+        numerical_column = ["writing_score" , "reading_score"] #  Seperated the numerial coulns 
+
+        input_featue_train_df = train_df.drop(columns= [target_column_name], axis= 1) # seperated the predectied feature just like X_train 
+        target_feature_train_df = train_df[target_column_name] # got the True Values in here just like y_train 
+
+        input_featue_test_df = test_df.drop(columns= [target_column_name], axis= 1) # seperated the test Output Feature just like X_test 
+    
+        target_feature_test_df = test_df[target_column_name] # seperatesd the true value just like y_test 
+
+
+        input_feature_train_arr = prerpocessing_obj.fit_transform(input_featue_train_df) # now processing the data as we do for  X_train 
+        input_feature_test_arr = prerpocessing_obj.transform(input_featue_test_df) # same as we for X_test data 
+
+
+        # we are now concatinating the Array after doing the Scaling and transforming the Data
+        train_arr = np.c_[
+            input_feature_test_arr , np.array(target_feature_train_df)
+        ]
+
+        test_arr = np.c_[
+            input_featue_test_df , np.array(target_feature_test_df)
+        ]
+
+        save_object(
+            file_path= self.data_transformation_config.preprocessor_obj_file,
+            obj= prerpocessing_obj
+        )
+
+        return(
+            train_arr,
+            test_arr,
+            self.data_transformation_config.preprocessor_obj_file
+
+        )
+
+
+
+   
+      
+
+    
--- src/utils.py
+++ src/utils.py
@@ -1,52 +1,61 @@
-import os 
+import os
 import sys
-import numpy as np
+
+import numpy as np 
 import pandas as pd
 import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
+from src.exception import CustomException
 
-def save_object (file_path , obj ):
-    
+def save_object(file_path, obj):
+    try:
         dir_path = os.path.dirname(file_path)
 
-        os.makedirs(dir_path , exist_ok= True)
+        os.makedirs(dir_path, exist_ok=True)
 
-        with open(file_path , 'wb') as file_obj :
-                pickle.dump(obj , file_obj )
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
 
-                
-
-def evluate_models(X_train , y_train , X_test , y_test, models , params) :
-
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+    try:
         report = {}
+
         for i in range(len(list(models))):
-                model = list(models.value())[i]
-                para = params[list(models.key())[i]]
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
-                gs = GridSearchCV(model , para , cv= 5)
-                gs.fit(X_train , y_train)
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
 
-                model.set_params(**gs.best_params_)
-                model.fit(X_train , y_train)
+            #model.fit(X_train, y_train)  # Train model
 
-                # Till Here the Traning part Completes
-                y_train_preds = model.predict(X_train)
-                y_test_preds = model.predict(X_test)
+            y_train_pred = model.predict(X_train)
 
-                train_model_score = r2_score(y_train , y_train_preds)
-                test_model_score = r2_score(y_test , y_test_preds)
+            y_test_pred = model.predict(X_test)
 
-                report[list(models.keys())[i]] = test_model_score
+            train_model_score = r2_score(y_train, y_train_pred)
 
-                return report 
-        
+            test_model_score = r2_score(y_test, y_test_pred)
 
+            report[list(models.keys())[i]] = test_model_score
 
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
 def load_object(file_path):
-        with open(file_path , "rb" ) as file_obj :
-                return pickle.load(file_obj)
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
-  
+    except Exception as e:
+        raise CustomException(e, sys)
